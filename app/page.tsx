3cb6d0b34ebed34c84029b9fdf73418a96764302
ediazjz <iam.ediazjz@gmail.com>
2022-12-11 23:08:54 -0600

    feat: configure supabase and fetch post in server

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { supabase } from "../lib";
 
-export default function Home() {
+export default async function Home() {
+  const { data: posts } = await supabase.from("posts").select();
+
   return (
-    <div className='bg-blue-900'>
+    <div className="bg-blue-900">
       <main>
         <h1>
           Welcome to <a href="https://nextjs.org">Next.js 13!</a>
         </h1>
 
         <p>
-          Get started by editing{' '}
-          <code>app/page.tsx</code>
+          Get started by editing <code>app/page.tsx</code>
         </p>
 
         <div>
@@ -19,9 +21,7 @@ export default function Home() {
             <p>Find in-depth information about Next.js 13</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-          >
+          <a href="https://github.com/vercel/next.js/tree/canary/examples">
             <h2>Examples &rarr;</h2>
             <p>Explore the Next.js 13 playground.</p>
           </a>
@@ -30,7 +30,6 @@ export default function Home() {
             href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-          
           >
             <h2>Deploy &rarr;</h2>
             <p>Deploy your Next.js site to a public URL with Vercel.</p>
@@ -44,12 +43,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
