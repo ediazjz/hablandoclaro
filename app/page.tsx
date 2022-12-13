d681b71c7210d7fe44ee871c26e6442ac6cb829b
ediazjz <iam.ediazjz@gmail.com>
2022-12-12 23:44:38 -0600

    feat: add Template component and font styling to Tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,19 @@
 import Image from "next/image";
+
+import { Template } from "../components/common";
 import { supabase } from "../lib";
 
 export default async function Home() {
   const { data: posts } = await supabase.from("posts").select();
 
   return (
-    <div className="bg-primary">
-      <main>
-        <h1 className="font-nunito">
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-        </h1>
-
-        <p className="font-josefin">
-          Get started by editing <code>app/page.tsx</code>
-        </p>
-
-        <div>
-          <a href="https://beta.nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/canary/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+    <Template>
+      <div className="h-96 w-full">hola</div>
+      <div className="h-96 w-full">hola</div>
+      <div className="h-96 w-full">hola</div>
+      <div className="h-96 w-full">hola</div>
+      <div className="h-96 w-full">hola</div>
+      <div className="h-96 w-full">hola</div>
+    </Template>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,31 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1.5rem",
+        sm: "1.75rem",
+        md: "2rem",
+        lg: "2.25rem",
+        xl: "2.5rem",
+        "2xl": "3rem",
+      },
+    },
+    fontSize: {
+      "2xs": ".625rem",
+      xs: ".75rem",
+      sm: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.75rem",
+      "4xl": "2rem",
+      "5xl": "2.25rem",
+      "6xl": "2.5rem",
+      "7xl": "2.75rem",
+    },
     extend: {
       fontFamily: {
         nunito: ["var(--font-nunito)"],
